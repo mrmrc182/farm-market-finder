0bca9768f58858b7cd79c5743d94d61413c9bac5
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-14 11:32:29 -0700

    additional work

--- style/js/searchpage.js
+++ style/js/searchpage.js
@@ -34,15 +34,6 @@ function init(){
         .then (function (data1){
             console.log(data1);
             console.log(data1.current.temp);
-            //market-details dynamic display
-            // temp.textContent = JSON.stringify(data1.current.temp);
-            // var img = document.createElement("img");
-            // var iconCode = data1.current.weather[0].icon;
-            // console.log(iconCode);
-            // var iconURL = "https://openweathermap.org/img/wn/" + iconCode + ".png";
-            // img.setAttribute("src", iconURL);
-            // weatherIcon.appendChild(img);
-            //search dynamic display
             searchTemp1.textContent = JSON.stringify(data1.current.temp);
             console.log(searchTemp1); 
             var iconCode1 = data1.current.weather[0].icon;
