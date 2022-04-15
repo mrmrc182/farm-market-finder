a6bd05aeb72181dfb3b4bc3774f60c5c9f3f594f
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-14 19:20:19 -0700

    pages now switch effortlessly

--- style/js/marketdetails.js
+++ style/js/marketdetails.js
@@ -1,9 +1,3 @@
-var returnToSearch = document.getElementById("returnBtn");
-
-returnToSearch.addEventListener("click", function(){
-    window.location.href = "./search.html?q=Encinitas";
-})
-
 var marketCity = document.getElementById("city");
 var marketName = document.getElementById("marketName");
 var marketLocation = document.getElementById("location");
@@ -14,63 +8,75 @@ var daysOpen = document.getElementById("daysOpen");
 var season1Months = document.getElementById("season1months");
 var season1Hours = document.getElementById("season1hours");
 var marketZip = document.getElementById("zipcode");
-var getMarketData = function(){
+var getMarketData = function () {
     var urlMarketID = document.location.search;
     var marketID = urlMarketID.split("=")[1];
     var marketsURL = "https://data.sandiegocounty.gov/resource/xazp-q2tj.json?id=" + marketID;
     fetch(marketsURL)
-    .then(function (response){
-        return response.json();
-    }) 
-    .then(function (data){
-        console.log(data);
-        console.log(data[0].city);
-        marketCity.textContent = data[0].city;
-        marketName.textContent = data[0].market_name;
-        marketLocation.textContent = " " + data[0].market_location;
-        marketManager.textContent = " " + data[0].market_manager;
-        marketManagerPhone.textContent = " " + data[0].market_manager_phone;
-        marketManagerEmail.textContent = " " + data[0].market_manager_email;
-        daysOpen.textContent = " " + data[0].days_open;
-        season1Months.textContent = " " + data[0].season_1_months + ": ";
-        season1Hours.textContent = data[0].season_1_hours;
-        marketZip.textContent = data[0].zip;
-        weatherIcon = document.getElementById("weatherIcon");
-        var getWeatherData = function(){
-            var weatherCity = marketCity.textContent;
-            var marketTemp = document.getElementById("marketTemp");
-            var weatherAPIURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + weatherCity + "&limit=3&appid=a914282a4259cce175b4a3f34d3738fb";
-            fetch (weatherAPIURL)
-            .then(function(response){
-                return response.json();
-            })
-            .then (function(data){
-                var geocodeLat = JSON.stringify(data[0].lat);
-                var geocodeLon = JSON.stringify(data[0].lon);
-                var nextURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geocodeLat + "&lon=" + geocodeLon + "&appid=a914282a4259cce175b4a3f34d3738fb&units=imperial";
-                fetch(nextURL)
-                .then(function(response){
-                    return response.json();
-                })
-                .then (function (data1){
-                    console.log(data1);
-                    marketTemp.textContent = JSON.stringify(data1.current.temp);  
-                    console.log(weatherIcon);
-                    var iconCode1 = data1.current.weather[0].icon;
-                    console.log(iconCode1);
-                    var iconURL = "https://openweathermap.org/img/wn/" + iconCode1 + ".png";
-                    console.log(iconURL);
-                    weatherIcon.setAttribute("src", iconURL);  
-                })
-            })
-            // insert it into the weather api
-            // displays appropriate temperature and icon     
-        }
-        getWeatherData();
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data[0].city);
+            marketCity.textContent = data[0].city;
+            marketName.textContent = data[0].market_name;
+            marketLocation.textContent = " " + data[0].market_location;
+            marketManager.textContent = " " + data[0].market_manager;
+            marketManagerPhone.textContent = " " + data[0].market_manager_phone;
+            marketManagerEmail.textContent = " " + data[0].market_manager_email;
+            daysOpen.textContent = " " + data[0].days_open;
+            season1Months.textContent = " " + data[0].season_1_months + ": ";
+            season1Hours.textContent = data[0].season_1_hours;
+            marketZip.textContent = data[0].zip;
+            weatherIcon = document.getElementById("weatherIcon");
+            var getWeatherData = function () {
+                var weatherCity = marketCity.textContent;
+                var marketTemp = document.getElementById("marketTemp");
+                var weatherAPIURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + weatherCity + "&limit=3&appid=a914282a4259cce175b4a3f34d3738fb";
+                fetch(weatherAPIURL)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        var geocodeLat = JSON.stringify(data[0].lat);
+                        var geocodeLon = JSON.stringify(data[0].lon);
+                        var nextURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geocodeLat + "&lon=" + geocodeLon + "&appid=a914282a4259cce175b4a3f34d3738fb&units=imperial";
+
+                        var returnToSearch = document.getElementById("returnBtn");
+                        returnToSearch.addEventListener("click", function () {
+                            window.location.href = "./search.html?q=" + marketCity;
+                        })
+                        var searchButton = document.getElementById("searchBtn");
+                        var inputType = document.getElementById("searchInput");
+                        searchButton.addEventListener("click", function (event) {
+                            event.preventDefault();
+                            sessionStorage.clear();
+                            var queryName = inputType.value.trim();
+                            console.log(queryName);
+                            window.location.href = "./search.html?q=" + queryName;
+                            sessionStorage.setItem("City", queryName);
+                            fetch(nextURL)
+                                .then(function (response) {
+                                    return response.json();
+                                })
+                                .then(function (data1) {
+                                    console.log(data1);
+                                    marketTemp.textContent = JSON.stringify(data1.current.temp);
+                                    console.log(weatherIcon);
+                                    var iconCode1 = data1.current.weather[0].icon;
+                                    console.log(iconCode1);
+                                    var iconURL = "https://openweathermap.org/img/wn/" + iconCode1 + ".png";
+                                    console.log(iconURL);
+                                    weatherIcon.setAttribute("src", iconURL);
+                                    // the search click event starts here
+                                })
+                        })
+                    })
+            }
+            getWeatherData();
+        })
 }
-
-
 getMarketData();
 
 // fetch  market data
